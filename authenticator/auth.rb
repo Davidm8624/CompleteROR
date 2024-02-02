44fec6925555232259496e3359592323213a3706
Davidm8624 <davidm8624@gmail.com>
2024-02-02 12:56:22 -0700

    part1

---
+++ authenticator/auth.rb
@@ -0,0 +1,19 @@
+db = [
+  {username: "name1", password: "password1"}
+  {username: "name2", password: "password2"}
+  {username: "name3", password: "password3"}
+  {username: "name4", password: "password4"}
+]
+
+
+
+while (i < 3) do
+
+
+puts "please sign in"
+25.times {print "-"}
+puts "this program will take input from the user and compare passwords"
+puts "if correct you will be returned with user object"
+
+
+end
