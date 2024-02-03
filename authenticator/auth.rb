52e7c42593a4692b902eee6fb7946bf328156a89
Davidm8624 <davidm8624@gmail.com>
2024-02-03 13:53:54 -0700

    threw rubocop at it

--- authenticator/auth.rb
+++ authenticator/auth.rb
@@ -1,45 +1,41 @@
 db = [
-{username: "name1", password: "password1"},
-{username: "name2", password: "password2"},
-{username: "name3", password: "password3"},
-{username: "name4", password: "password4"},
+  { username: 'name1', password: 'password1' },
+  { username: 'name2', password: 'password2' },
+  { username: 'name3', password: 'password3' },
+  { username: 'name4', password: 'password4' }
 ]
 
 def auth_user(username, password, user_list)
-		user_list.each do |user|
-		if user[:username] == username && user[:password] == password
-			return user
-			break
-			
-		end
-	end
-	return "that is not the correct combination"
+  user_list.each do |user|
+    next unless user[:username] == username && user[:password] == password
+
+    return user
+    break
+  end
+  'that is not the correct combination'
 end
 
+puts 'please sign in'
+25.times { print '-' }
+puts 'this program will take input from the user and compare passwords'
+puts 'if correct you will be returned with user object'
 
-puts "please sign in"
-25.times {print "-"}
-puts "this program will take input from the user and compare passwords"
-puts "if correct you will be returned with user object"
-
-attempts = 1;
+attempts = 1
 
 while attempts < 4
-	print "Username: "
-	username = gets.chomp
-	print "Password: "
-	password = gets.chomp
-	auth_response = auth_user(username, password, db)
-	puts auth_response
-	puts "Press N to quit or any other key to contiune"
-	input = gets.chomp.downcase
-	if input == "n"
-		break
-	end
-	attempts += 1
+  print 'Username: '
+  username = gets.chomp
+  print 'Password: '
+  password = gets.chomp
+  auth_response = auth_user(username, password, db)
+  puts auth_response
+  puts 'Press N to quit or any other key to contiune'
+  input = gets.chomp.downcase
+  break if input == 'n'
+
+  attempts += 1
 end
 
-
-if attempts = 4
-	puts "attempts have reached max, please try again later"
+if attempts == 4
+  puts 'attempts have reached max, please try again later'
 end
