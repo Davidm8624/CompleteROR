e929bded4030451e1fa3b01aa5abb57917c45f7f
davidm8624 <davidm8624@gmail.com>
2024-03-02 20:26:06 -0700

    rubocop'd it

--- school_obj_proj/student.rb
+++ school_obj_proj/student.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Student
   attr_accessor :first_name, :last_name, :email, :username, :password
 
@@ -12,18 +14,15 @@ class Student
   def to_s
     "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
                   email address: #{@email}"
-  end  
-  
+  end
 end
 
-mashrur = Student.new("Mashrur", "Hossain", "mashrur1", "mashrur@example.com",
-                      "password1")
-john = Student.new("John", "Doe", "john1", "john1@example.com",
-                      "password2")
+mashrur = Student.new('Mashrur', 'Hossain', 'mashrur1', 'mashrur@example.com',
+                      'password1')
+john = Student.new('John', 'Doe', 'john1', 'john1@example.com',
+                   'password2')
 puts mashrur
 puts john
 mashrur.last_name = john.last_name
-puts "Mashrur is altered"
+puts 'Mashrur is altered'
 puts mashrur
-
-
